a6fa9488289f10b3e57799057fb4b5169cb08789
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-04-08 21:40:02 +0300

    basic example added

---
+++ basic/src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include "mpi.h"
+
+static const unsigned MAX = 100U;
+
+int main(int argc, char **argv)
+{
+  int rank, size;
+
+  /* Инициализация среды MPI */
+  MPI_Init(&argc, &argv);
+  
+  /* Общее число процессов в коммуникаторе */
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  
+  /* Ранг процесса */
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  
+  int n = (MAX - 1) / (size + 1);
+  
+  int ibeg = rank * n + 1;
+  int iend = (rank + 1) * n;
+  
+  for(int i = ibeg; i <= ((iend > MAX)? MAX : iend); i++) {
+    std::cout << "Process " << rank << ", " << i << "^2 = " << i*i << "\n";
+  }
+    
+  /* Остановка среды MPI */
+  MPI_Finalize();
+}
