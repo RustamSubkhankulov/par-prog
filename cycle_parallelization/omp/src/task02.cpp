1f3d02dfae95d77b2a09650015e1f91c6a5ee059
Rustam Subkhankulov <subkhankulov@ispras.ru>
2024-11-28 20:54:03 +0300

    cycle_parallelization/omp: task02 in progress.

--- cycle_parallelization/omp/src/task02.cpp
+++ cycle_parallelization/omp/src/task02.cpp
@@ -13,10 +13,10 @@
 namespace
 {
 /* Array dimensions. */
-const int Isize = 40000;
-const int Jsize = 40000;
+const int Isize = 5000;
+const int Jsize = 5000;
 
-using row = double[Jsize];
+using row = double[Jsize + 3];
 } /* anonymous namespace */
 
 int main()
@@ -37,14 +37,29 @@ int main()
   sw.start();
 #endif
 
-/* Main computational cycle. */
-#pragma omp parallel default(none) shared(a)
-#pragma omp for schedule(static)
+  /* Duplicate data for extra columns. */
   for (int i = 0; i < Isize; ++i)
   {
-    for (int j = 0; j < Jsize; ++j)
+    for (int j = Jsize; j < Jsize + 3; ++j)
     {
-      a[i][j] = sin(2 * a[i][j]);
+      a[i][j] = a[i][j - 3];
+    }
+  }
+
+  /* Main computations. */
+  for (int i = 2; i < 4; ++i)
+  {
+    for (int j = 0; j < Jsize - 3; ++j)
+    {
+      a[i][j + 3] = 2 * a[i - 2][j + 3];
+    }
+  }
+
+  for (int i = 4; i < Isize; ++i)
+  {
+    for (int j = 0; j < Jsize - 3; ++j)
+    {
+      a[i][j + 3] = sin(5 * a[i - 2][j + 6]);
     }
   }
 
@@ -56,9 +71,19 @@ int main()
   auto result_file = std::ofstream("result.txt");
   assert(result_file.is_open());
 
-  for (int i = 0; i < Isize; ++i)
+  for (int i = 0; i < 2; ++i)
   {
     for (int j = 0; j < Jsize; ++j)
+    {
+      result_file << a[i][j] << ' ';
+    }
+
+    result_file << std::endl;
+  }
+
+  for (int i = 2; i < Isize; ++i)
+  {
+    for (int j = 3; j < Jsize + 3; ++j)
     {
       result_file << a[i][j] << ' ';
     }
